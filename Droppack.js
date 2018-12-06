c97d78b6fda41297d9444d2efa6531b626fcfd24
mmongy <marc.mongy@hotmail.fr>
2018-12-06 21:37:23 +0100

    Add files via upload

--- Droppack.js
+++ Droppack.js
@@ -1,1 +1,33 @@
+class Droppack {
+  constructor(x, y) {
+    let sprite = getImage("creatures/Hopper-Happy");;
+    let height = sprite.height;
+    let width = sprite.width;
+    this.x = x;
+    this.y = y;
+  }
 
+  function getHeight(){
+    return height;
+  }
+
+  function getWidth(){
+    return width;
+  }
+
+  function getX(){
+    return x;
+  }
+
+  function getY(){
+    return y;
+  }
+
+  function droppackHitbox(Droppack droppack){
+    let height = droppack.getHeight();
+    let width = droppack.getWidth();
+    let area = height*width;
+    return area
+  }
+
+}
