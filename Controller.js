9be08b4a79956e02ce9d89660decf732f1a50f2e
Thblanc <33553888+Kalarth@users.noreply.github.com>
2018-12-06 22:08:06 +0100

    Add files via upload

--- Controller.js
+++ Controller.js
@@ -1,1 +1,31 @@
+class Controller{
+  constructor(){
+    this.grid = new Array(1000);
+    for (var i = 0; i < this.grid.length; i++) {
+      this.grid[i] = new Array(1000);
+    }
+    this.player = new Bird(10,10);
+    //this.grid = new grid(args);
+    this.obstacleList = [];
+    this.objectivelist = [];
+    this.droppackageList = [];
+  }
+    run = function(){
+      this.first_init();
+       while (true) {
+         //Check control entry
+         //if yes, bird.dothing(0)
+         this.actualise();
+         //scrolling happens
+         this.actualise();
 
+
+       }
+    }
+
+  function first_init(){
+    let background = new Image();
+    background.src = "lib/background.png";
+
+  }
+}
