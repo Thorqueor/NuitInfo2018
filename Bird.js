e6d1fbf1fccf7518ed4c1824575a69e68688a183
Thorqueor <32955184+Thorqueor@users.noreply.github.com>
2018-12-06 22:07:03 +0100

    Add files via upload
    bird v0.0.1

--- Bird.js
+++ Bird.js
@@ -1,1 +1,44 @@
+class Bird extends Actor{
+  constructor(x,y) {
+    var sprite = new Image();
+    sprite.src = "creatures/Hopper-Happy";
+    var heigth = sprite.heigth;
+    var width = sprite.width;
+    super(x,y);
+    dead = false;
+  }
+}
 
+function getCoordonnees(){
+  return x,y;
+}
+
+function getHitbox(){
+  return heigth*width;
+}
+
+function up(){
+  if (keyIsPressed && keyCode === 38) {
+        y=y+5;
+    }
+}
+
+function down(){
+  if (keyIsPressed && keyCode === 40) {
+        y=y-5;
+    }
+}
+
+function left(){
+  if (keyIsPressed && keyCode === 37) {
+        x=x-5;
+    }
+}
+
+function right(){
+  if (keyIsPressed && keyCode === 39) {
+        x=x+5;
+    }
+}
+
+let myzozio = new Bird(10,10);
